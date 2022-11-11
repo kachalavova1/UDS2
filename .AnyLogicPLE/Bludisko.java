b40a6a371e93d7d0c0be1359500767a5b416a9da
Nikol112 <nikolabrenkusova@gmail.com>
2022-11-11 15:16:21 +0100

    Vytvorenie triedy Bludisko

---
+++ .AnyLogicPLE/Bludisko.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Bludisko here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bludisko {
+    /**
+     * Constructor for objects of class Bludisko
+     */
+    public Bludisko() {
+        // initialise instance variables
+    }
+}
